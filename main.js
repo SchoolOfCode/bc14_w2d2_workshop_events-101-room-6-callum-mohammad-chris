fb2ffd4ce43a2b1cce52ada67581ffe750aafad6
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-03-21 11:49:33 +0000

    Task 1 Complete

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+
+
+
 const button = document.querySelector("#click-me");
 
 function handleClick(event) {
   // If clicked, console log you clicked me!
+  console.log('You Clicked Me!')
+  console.log(event)
+  if(event.shiftKey===true){
+    event.target.textContent = "NAILED IT!"
+  }
+  else{
+    event.target.textContent = "Click Me..."
+  }
   // If shift key pressed, change the inner text of the button to be NAILED IT
 }
 
