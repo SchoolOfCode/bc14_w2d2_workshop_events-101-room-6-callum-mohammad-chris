3e49a327c527f05e5c5366f159afba5b4618dc42
Chris Branson <chrisbranson@live.co.uk>
2023-04-13 20:28:45 +0100

    Task 4 done

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ button.addEventListener("click", handleClick);
 function handleClick(event) {
   console.log("You clicked me!")
   console.log(event)
-  if (event.shiftKey){
-	button.textContent = "Hi Anna"
-	document.querySelector("h1").textContent = "Hi Anna"
-	document.title = "Hi Anna"
+ 	button.textContent = "Hi Maggie"
+	document.querySelector("h1").textContent = "Hi Maggie! Move the mouse over the picture and try typing in the box below!"
+	document.title = "Hi Maggie"
   }
-}
+
 
 const flowerButton = document.querySelector(".task-2")
 flowerButton.addEventListener("click", changeTitle)
@@ -21,7 +20,7 @@ function changeTitle(event){
 
 let titleInput = document.querySelector("#title-changer")
 titleInput.addEventListener("keyup", changeHeading )
-document.querySelector("#title-changer").placeholder =("Type in me Anna!")
+document.querySelector("#title-changer").placeholder =("Type in me Maggie!")
 
 function changeHeading(event){
 	document.querySelector("h1").textContent = event.target.value
@@ -30,3 +29,15 @@ function changeHeading(event){
 document.querySelector("#title-changer").addEventListener("keyup", (e) => {
   console.log(e.target.value);
 });
+
+let image = document.querySelector("img")
+image.addEventListener("mouseenter", changeColour)
+image.addEventListener("mouseleave", changeColourBack)
+
+function changeColour(event){
+  document.querySelector("h1").style.color = "hotpink"
+}
+
+function changeColourBack(event){
+  document.querySelector("h1").style.color = "initial"
+}
