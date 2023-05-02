f547eca05f4d10fe0e55f86967da5c0198f5dba8
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-05-02 15:50:31 +0100

    Task 4 Done

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const button = document.querySelector("#click-me");
+const flowerButton = document.querySelector(".task-2")
+const header = document.querySelector("h1")
+const img = document.querySelector("img")
 
 function handleClick(event) {
-  // If clicked, console log you clicked me!
-  // If shift key pressed, change the inner text of the button to be NAILED IT
+  console.log("You Clicked Me!")
+  console.dir(button)
+  console.dir(flowerButton)
+  if(event.shiftKey === true){
+    event.target.textContent = 'NAILED IT!';
+  }else{
+    event.target.textContent = "Click Me?"
+  }
+}
+function handleFlowerClick(event){
+  document.title = "💐🌷🌼"
+}
+function handleCursorEnterImage(event){
+
+  header.style.color = "hotpink"
+}
+function handleCursorLeaveImage(event){
+
+  header.style.color = "initial"
 }
 
 button.addEventListener("click", handleClick);
+flowerButton.addEventListener("click",handleFlowerClick)
+img.addEventListener("mouseenter", handleCursorEnterImage)
+img.addEventListener("mouseleave", handleCursorLeaveImage)
 
-document.querySelector("#title-changer").addEventListener("keyup", (e) => {
-  console.log(e.target.value);
+document.querySelector("#title-changer").addEventListener("keyup", (event) => {
+  console.log(event.target.value);
+  header.textContent = event.target.value
 });
+
